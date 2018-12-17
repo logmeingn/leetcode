ea5c9a09e0a8d5975b366a5e151b5b228a8c524f
logmeingn <logmeingn@gmail.com>
2018-12-17 11:37:17 -0600

    Crude solution with O(n^2)

--- twoSum/twosum.cpp
+++ twoSum/twosum.cpp
@@ -1,24 +1,39 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 class Solution{
    public:
-      std::vector<int> twoSum(std::vector<int>& list, int target)
+      std::vector<int> twoSum(const std::vector<int>& list, int targetSum)
       {
-	 std::vector<int> indices{1,2};
-
+	 std::vector<int> indices;
+	 int ii = 0;
+	 for(auto i = list.cbegin(); i != list.end(); i++, ii++)
+	 {
+	    int jj = ii+1;
+	    for(auto j = i+1; j != list.end(); j++, jj++)
+	    {
+	       std::cout << "list[" << *i << "," << *j << "]" << std::endl;
+	       if ( targetSum == *i + *j )
+	       {
+		  indices.push_back(ii);
+		  indices.push_back(jj);
+	       }
+	    }
+	 }
 	 return indices;
       }
 };
 
 int main(void)
 {
-   Solution s;
-   std::vector<int> list{1,2,3,4};
-   std::vector<int> indices = s.twoSum(list, 5);
+   std::vector<int> list = {1,2,3,4};
 
-   for (const int& i : indices)
+   Solution s;
+   std::vector<int> indices = s.twoSum(list, 6);
+
+   for (auto index : indices)
    {
-      std::cout << "i:" << i << std::endl;
+      std::cout << "index:" << index << std::endl;
    }
+   return 0;
 }
