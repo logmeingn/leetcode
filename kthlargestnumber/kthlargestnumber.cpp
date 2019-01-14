2ac01eaa0825e2d98006400ca832d6ca649501b4
garanaveen <garanaveen@gmail.com>
2019-01-13 19:14:38 -0600

    Not complete yet. Logic seems complete but need to fix the while loop in findKthLargest

--- kthlargestnumber/kthlargestnumber.cpp
+++ kthlargestnumber/kthlargestnumber.cpp
@@ -1,20 +1,78 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int length = nums.length();
-        book kreached = false;
-        int currentk = 1;
-        int currentMax = nums.at(0);
-        for (int i = 1 ; i < length ; i++)
+        int length = nums.size();
+        int pivotPlace = 0;
+        int lesserNumberIndex = 0;
+        int higherNumberIndex = length-1;
+        //while (pivotPlace != k)
+        int i = 0;
+        pivotPlace = placePivot(nums, lesserNumberIndex,higherNumberIndex);
+        while ( i < 2)
         {
-            if (currentMax < nums.at(i))
+            i++;
+            if(pivotPlace < k)
             {
-                currentk++;
-                currentMax = nums.at(i);
+                higherNumberIndex = pivotPlace-1;
+                pivotPlace = placePivot(nums, lesserNumberIndex,higherNumberIndex);
             }
-            else
+            else if(pivotPlace > k)
             {
+                lesserNumberIndex = pivotPlace+1;
+                pivotPlace = placePivot(nums, lesserNumberIndex, higherNumberIndex);
             }
         }
+        return pivotPlace;
+    }
+    
+    void printNums(const vector<int>& nums)
+    {
+        for (int i = 0 ; i < nums.size(); i++)
+        {
+            cout << nums.at(i) << ",";
+        }
+        cout << endl;
+    }
+    
+    int placePivot(vector<int>& nums, int lesserNumberIndex, int higherNumberIndex)
+    {
+        cout << "----------------------------" << endl;
+        cout << "higherNumberIndex :" << higherNumberIndex << endl;
+        cout << "lesserNumberIndex :" << lesserNumberIndex << endl;
+        printNums(nums);
+        
+        int pivotPos = 0;
+        int i = 0;
+        while ((higherNumberIndex >= lesserNumberIndex))
+        {
+            if (nums.at(pivotPos) < nums.at(higherNumberIndex) && (pivotPos < higherNumberIndex))
+            {
+                //swap values
+                int tmpi = nums[pivotPos];
+                nums[pivotPos] = nums[higherNumberIndex];
+                nums[higherNumberIndex] = tmpi;
+                pivotPos = higherNumberIndex;
+                higherNumberIndex--;
+            }
+            else if((nums[pivotPos] > nums[lesserNumberIndex]) && (pivotPos > lesserNumberIndex) )
+            {
+                                //swap values
+                int tmpi = nums[pivotPos];
+                nums[pivotPos] = nums[lesserNumberIndex];
+                nums[lesserNumberIndex] = tmpi;
+                pivotPos = lesserNumberIndex;
+                lesserNumberIndex++;
+            }
+            
+            //cout << "Iteration : " << i++ << ":";
+            //printNums(nums);
+           //cout << "("<< pivotPos << ")"<< "("<< lesserNumberIndex << "," << higherNumberIndex << ")" << endl;
+        }
+        
+            //cout << endl << "Final : " ;
+            //printNums(nums);
+        
+       cout << endl << nums[pivotPos] << "is at " << pivotPos << endl;
+        return pivotPos;
     }
 };
